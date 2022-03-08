1a23e09cb21b613bfcbca410d9bc5d2adeb85866
jkalandarov <jkalandarov@gmail.com>
2022-03-08 13:52:56 +0500

    Page testing set up

--- travel.js
+++ travel.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express()
-const handlebars = require('express-handlebars')
-.create({defaultLayout: 'main'})
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
 const fortune = require('./lib/fortune')
-
-app.set('port', process.env.PORT || 5000)
-app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) => {
-    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
-    next();
-})
 
 //Set up handlebars view engine
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
-app.get('/', function(req, res){
-    res.render('home');
+app.set('port', process.env.PORT || 5000)
+app.use(express.static(__dirname + '/public'));
+
+//set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
 });
 
-app.get('/about', function(req, res){
-    res.render('about', {fortune: fortune.getFortune()});
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
 
 // custom 404 page
