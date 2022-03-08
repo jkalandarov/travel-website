3cb4a387ca1c9fa8ceacdb37a5ff12d7ac783b45
jkalandarov <jkalandarov@gmail.com>
2022-03-08 17:34:21 +0500

    Group rate request and Hood river pages created

--- travel.js
+++ travel.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
 const fortune = require('./lib/fortune')
@@ -27,6 +28,14 @@ app.get('/about', function(req,res){
 		pageTestScript: '/qa/tests-about.js' 
 	} );
 });
+
+app.get('/tours/hood-river', (req, res) => {
+    res.render('tours/hood-river')
+})
+
+app.get('/tours/request-group-rate', (req, res) => {
+    res.render('tours/request-group-rate');
+})
 
 // custom 404 page
 app.use(function(req, res){
